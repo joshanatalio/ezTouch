55f8404a158ead181bf635a56980b848e4e3c07f
joshanatalio <josh@anatalio.net>
2014-11-08 13:28:25 -0800

    working on cells

--- ezTouch/ViewController.swift
+++ ezTouch/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet var mainCellView: UIView!
+    @IBOutlet var nameLabel:UILabel!
+    @IBOutlet var touchButton:UIButton!
+    @IBOutlet var settingButton:UIButton!
+    
+    
 }
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+ @IBOutlet var tableView: UITableView!
+    var items: [String] = ["We", "Heart", "Swift"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.rowHeight = 250
+        self.tableView.backgroundView = UIImageView(image: UIImage(named: "Background"))
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        var mainCellView = UIView(frame: CGRectMake(100, 200, 250, 250))
+        cell.backgroundColor = UIColor(red:0.0, green:0.0,blue:0.0,alpha:0.1)// transparent color
+        cell.textLabel.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+}
